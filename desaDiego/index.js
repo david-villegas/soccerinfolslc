8f7305ce3d280867926856be1a77406b6d1a9e1d
diegudeveloper <diegudeveloper@gmail.com>
2022-07-10 22:16:19 +0200

    Conf carrusel section ultmos resultados

--- desaDiego/index.js
+++ desaDiego/index.js
@@ -20,6 +20,11 @@ var seccionesPagina = new fullpage('#fullpage', {
 	navigationTooltips: ['Inicio', 'Ultimos Resultados', 'Proximos Juegos', 'Contacto'], // Tooltips que mostrara por cada boton.
 	showActiveTooltip: false, // Mostrar tooltip activa.
 	
+	// ──────────────────────────────────────────────────
+	//   :::::: Secciones
+	// ──────────────────────────────────────────────────
+	sectionsColor : ['#000', '#c2c2c2', '#000'], // Color de fondo de cada seccion.
+	verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
 	
 });
 
